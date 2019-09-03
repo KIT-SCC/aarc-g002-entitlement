06d59b663c823cc31a40f19b95493a9019216b80
Marcus <marcus@hardt-it.de>
2019-09-03 15:35:40 +0200

    do rename for local tests

--- aarc_g002_entitlement/aarc_g002_entitlement.py
+++ aarc_g002_entitlement/aarc_g002_entitlement.py
@@ -60,7 +60,7 @@ class Aarc_g002_entitlement :
     def __repr__(self):
         """Serialize the entitlement to the AARC-G002 format.
 
-        This is the inverse to `__init__` and thus `ent_str == repr(Aarc_g002_ePE(ent_str))`
+        This is the inverse to `__init__` and thus `ent_str == repr(Aarc_g002_entitlement(ent_str))`
         holds for any valid entitlement.
         """
         return ((
@@ -79,7 +79,7 @@ class Aarc_g002_entitlement :
     def __str__(self):
         """Return the entitlement in human-readable string form."""
         return ((
-            '<Aarc_g002_ePE' +
+            '<Aarc_g002_entitlement' +
             ' namespace={namespace_id}:{delegated_namespace}{subnamespaces}' +
             ' group={group}{subgroups}' +
             '{role}' +
@@ -180,8 +180,8 @@ class Aarc_g002_entitlement :
 if __name__ == '__main__':
     required_group= 'urn:geant:h-df.de:group:aai-admin:role=member#unity.helmholtz-data-federation.de'
     actual_group  = 'urn:geant:h-df.de:group:aai-admin:role=member#unity.helmholtz-data-federation.de'
-    required_entitlement = Aarc_g002_ePE(required_group)
-    actual_entitlement   = Aarc_g002_ePE(actual_group)
+    required_entitlement = Aarc_g002_entitlement(required_group)
+    actual_entitlement   = Aarc_g002_entitlement(actual_group)
     print('\n1: Simple case: Different authorities, everything else same')
     print('    Required group: ' + required_group)
     print('    Actual   group: ' + actual_group)
@@ -191,8 +191,8 @@ if __name__ == '__main__':
 
     required_group= 'urn:geant:h-df.de:group:aai-admin:role=member#unity.helmholtz-data-federation.de'
     actual_group  = 'urn:geant:h-df.de:group:aai-admin:role=member#backupserver.used.for.developmt.de'
-    required_entitlement = Aarc_g002_ePE(required_group)
-    actual_entitlement   = Aarc_g002_ePE(actual_group)
+    required_entitlement = Aarc_g002_entitlement(required_group)
+    actual_entitlement   = Aarc_g002_entitlement(actual_group)
 
     print('\n2: Simple case: Different authorities, everything else same')
     print('    Required group: ' + required_group)
@@ -203,8 +203,8 @@ if __name__ == '__main__':
 
     required_group= 'urn:geant:h-df.de:group:aai-admin#unity.helmholtz-data-federation.de'
     actual_group  = 'urn:geant:h-df.de:group:aai-admin:role=member#backupserver.used.for.developmt.de'
-    required_entitlement = Aarc_g002_ePE(required_group)
-    actual_entitlement   = Aarc_g002_ePE(actual_group)
+    required_entitlement = Aarc_g002_entitlement(required_group)
+    actual_entitlement   = Aarc_g002_entitlement(actual_group)
 
     print('\n3: Role assigned but not required')
     print('    Required group: ' + required_group)
@@ -215,8 +215,8 @@ if __name__ == '__main__':
 
     required_group= 'urn:geant:h-df.de:group:aai-admin:role=member#unity.helmholtz-data-federation.de'
     actual_group  = 'urn:geant:h-df.de:group:aai-admin#backupserver.used.for.developmt.de'
-    required_entitlement = Aarc_g002_ePE(required_group)
-    actual_entitlement   = Aarc_g002_ePE(actual_group)
+    required_entitlement = Aarc_g002_entitlement(required_group)
+    actual_entitlement   = Aarc_g002_entitlement(actual_group)
 
     print('\n4: Role required but not assigned')
     print('    Required group: ' + required_group)
@@ -227,8 +227,8 @@ if __name__ == '__main__':
 
     required_group= 'urn:geant:h-df.de:group:aai-admin:special-admins#unity.helmholtz-data-federation.de'
     actual_group  = 'urn:geant:h-df.de:group:aai-admin#backupserver.used.for.developmt.de'
-    required_entitlement = Aarc_g002_ePE(required_group)
-    actual_entitlement   = Aarc_g002_ePE(actual_group)
+    required_entitlement = Aarc_g002_entitlement(required_group)
+    actual_entitlement   = Aarc_g002_entitlement(actual_group)
 
     print('\n5: Subgroup required, but not available')
     print('    Required group: ' + required_group)
@@ -238,8 +238,8 @@ if __name__ == '__main__':
 
     required_group= 'urn:geant:h-df.de:group:aai-admin#unity.helmholtz-data-federation.de'
     actual_group  = 'urn:geant:h-df.de:group:aai-admin:testgroup:special-admins#backupserver.used.for.developmt.de'
-    required_entitlement = Aarc_g002_ePE(required_group)
-    actual_entitlement   = Aarc_g002_ePE(actual_group)
+    required_entitlement = Aarc_g002_entitlement(required_group)
+    actual_entitlement   = Aarc_g002_entitlement(actual_group)
 
     print('\n6: Edge case: User in subgroup, but only supergroup required')
     print('    Required group: ' + required_group)
@@ -250,8 +250,8 @@ if __name__ == '__main__':
 
     required_group= 'urn:geant:h-df.de:group:aai-admin:role=admin#unity.helmholtz-data-federation.de'
     actual_group  = 'urn:geant:h-df.de:group:aai-admin:special-admins:role=admin#backupserver.used.for.developmt.de'
-    required_entitlement = Aarc_g002_ePE(required_group)
-    actual_entitlement   = Aarc_g002_ePE(actual_group)
+    required_entitlement = Aarc_g002_entitlement(required_group)
+    actual_entitlement   = Aarc_g002_entitlement(actual_group)
 
     print('\n7: role required for supergroup but only assigned for subgroup')
     print('    Required group: ' + required_group)
@@ -259,9 +259,6 @@ if __name__ == '__main__':
     print('    is_contained_in:   => {}'.format(required_entitlement.is_contained_in(actual_entitlement)))
     print('        (are equal:    => {})'.format(required_entitlement == actual_entitlement))
 
-
-    print ('\n\n{}\n{}'.format(actual_entitlement.__repr__, actual_entitlement.__repr2__))
-    print ('\n\n{}\n{}'.format(actual_entitlement.__repr__(), actual_entitlement.__repr2__()))
 
 
     #TODO: Add more Weird combinations of these with roles
