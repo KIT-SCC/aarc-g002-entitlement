600ac273e5c9277627f3c26d1fd5c87272f2ef17
Lukas Burgey <lukas.burgey@kit.edu>
2020-04-03 13:03:11 +0200

    Add the logo path to conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -106,7 +106,9 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = [
+    'logo.png',
+]
 
 # Add any paths that contain "extra" files, such as .htaccess or
 # robots.txt.
